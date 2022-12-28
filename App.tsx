705a505b4d54929d63bd84b86d0021da72e1b894
Nabeel Iqbal <nabi_iqbal@yahoo.com>
2022-12-28 20:22:00 +0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { _ANSWER_TO_EVERYTHING, AuthWebView, WebViewMessage } from "webxauth";
 
 export default function App() {
+  /*
+   *    BEGIN _ WebView Methods
+   */
+  // const onPostMessage = async (message: WebViewMessage) => {
+  //   console.log("App ::: onPostMessage ::: message", JSON.stringify(message));
+
+  //   return true;
+  // };
+
+  const onPostAction = async (action: string) => {
+    console.log("App ::: onPostAction ::: action", action);
+  };
+
+  const onPostData = async (data: object) => {
+    const stringifiedData = JSON.stringify(data);
+    console.log("onPostData ::: stringifiedData", stringifiedData);
+  };
+
+  const onClickMetaMask = async () => {};
+
+  const onClickWalletConnect = async () => {};
+
+  const onErrorAuthWebView = async (error: unknown) => {
+    console.log("error", error);
+  };
+  /*
+   *    END _ WebView Methods
+   */
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30 }}>Test: {_ANSWER_TO_EVERYTHING}</Text>
+      <AuthWebView
+        onClickMetaMask={onClickMetaMask}
+        onClickWalletConnect={onClickWalletConnect}
+        onPostAction={onPostAction}
+        onPostData={onPostData}
+        onPostMessage={onPostMessage}
+        onError={onErrorAuthWebView}
+        delay={1000}
+        backgroundColor={"#eaeaea"}
+        dependencies={[]}
+      />
     </View>
   );
 }
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
