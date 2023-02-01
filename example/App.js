21042783fcf5410bb607420c5fab9f22bf170f60
Oliver Molnar <403access@olimo.me>
2023-02-01 15:50:31 +0400

    feat: render app within safe area on screen - use React Native SafeAreaView

--- example/App.js
+++ example/App.js
@@ -1,1 +1,24 @@
-export { default } from './src/App'
+/*
+ *     FRAMEWORK
+ */
+import { SafeAreaView, StyleSheet } from 'react-native'
+/*
+ *     COMPONENTS - App
+ */
+import App from './src/App'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+})
+
+export default (
+  <SafeAreaView style={styles.container}>
+    <App />
+  </SafeAreaView>
+)
